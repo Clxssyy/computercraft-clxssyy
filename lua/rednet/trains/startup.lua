f7e562af6b593d03f95ac766f7ca02fb31ea50b3
Michael <mpc63@uakron.edu>
2024-05-16 02:31:16 -0400

    Fix command system

--- lua/rednet/trains/startup.lua
+++ lua/rednet/trains/startup.lua
@@ -14,11 +14,17 @@ stationName = station.getStationName()
 monitor = peripheral.wrap("top")
 barrel = peripheral.wrap("back")
 
+command = {}
+
 -- [[UTILS]] --
 function error(message)
-  term.setTextColor(colors.red)
-  print("[!] " .. message)
-  term.setTextColor(textColor)
+  if id then
+    rednet.send(id, "ERROR - " .. message, protocol)
+  else
+    term.setTextColor(colors.red)
+    print("[!] " .. message)
+    term.setTextColor(textColor)
+  end
 end
 
 function split(input, separator)
@@ -39,10 +45,24 @@ function displayHeader()
   io.write(" }=-----\n")
 end
 
+function displayMessage(message)
+  if id then
+    rednet.send(id, message, protocol)
+  else
+    print(message)
+  end
+end
+
 -- [[COMMANDS]] --
 -- Monitor
-function updateMonitor(text)
-  if not text then
+function setMonitorText(text)
+  if text then
+    local newText = ""
+    for i = 1, #text do
+      newText = newText .. text[i] .. " "
+    end
+    text = newText
+  else
     text = stationName
   end
 
@@ -50,10 +70,17 @@ function updateMonitor(text)
   monitor.setCursorPos(1, 1)
   monitor.setTextColor(textColor)
   monitor.write(text)
+
+  displayMessage("Monitor text set to: " .. text)
 end
 
--- Train
+-- Station
 function setSchedule(requestedSchedule)
+  if not requestedSchedule then
+    error("No schedule provided")
+    return
+  end
+
   local items = barrel.list()
 
   if #items == 0 then
@@ -64,7 +91,7 @@ function setSchedule(requestedSchedule)
       local item = barrel.getItemDetail(i)
       if item.name == "create:schedule" then
         if item.displayName == requestedSchedule then
-          return item
+          station.setSchedule(item)
         end
       end
     end
@@ -72,37 +99,118 @@ function setSchedule(requestedSchedule)
   end
 end
 
+function trainAssemble()
+  if station.isInAssemblyMode() == false then
+    error("Station is not in assembly mode")
+    return
+  end
+  station.assemble()
+end
+
+function toggleAssemblyMode()
+  station.setAssemblyMode(not station.isInAssemblyMode())
+end
+
+function trainDisassemble()
+  if station.isInAssemblyMode() == true then
+    error("Station is in assembly mode")
+    return
+  end
+  station.disassemble()
+end
+
 -- Utils
 function clear()
   term.clear()
   term.setCursorPos(1, 1)
   displayHeader()
+
+  if id then
+    rednet.send(id, "Screen cleared", protocol)
+  end
 end
 
 registeredCommands = {
   [1] = {
     name = "clear",
-    action = clear
+    handler = clear
+  },
+  [2] = {
+    name = "monitor",
+    options = {
+      [1] = {
+        name = "set",
+        options = {
+          [1] = {
+            name = "text",
+            handler = setMonitorText
+          }
+        }
+      },
+    },
+  },
+  [3] = {
+    name = "station",
+    options = {
+      [1] = {
+        name = "set",
+        options = {
+          [1] = {
+            name = "station",
+            handler = setSchedule
+          }
+        }
+      },
+      [2] = {
+        name = "assemble",
+        handler = trainAssemble
+      },
+      [3] = {
+        name = "toggle",
+        handler = toggleAssemblyMode
+      },
+      [4] = {
+        name = "disassemble",
+        handler = trainDisassemble
+      }
+    }
   }
 }
 
-function executeCommand(commandName)
-  for i = 1, #registeredCommands do
-    if registeredCommands[i].name == commandName then
-      registeredCommands[i].action()
-      return
+commandList = {}
+
+function executeCommand(command, commandList)
+  for i = 1, #commandList do
+    if command[1] == commandList[i].name then -- Command found
+      if commandList[i].handler then -- Check for a handler
+        if #command == 1 then -- Command has no arguments
+          commandList[i].handler()
+          return
+        elseif #command > 1 and not commandList[i].options then -- Command has arguments
+          table.remove(command, 1)
+          commandList[i].handler(command)
+          return
+        end
+      end
+      if commandList[i].options then -- heck for options
+        if #command == 1 then
+          error("Usage error") -- No options provided
+          return
+        end
+
+        commandList = commandList[i].options
+        table.remove(command, 1)
+        executeCommand(command, commandList)
+        return
+      end
     end
   end
-
-  if id then
-    rednet.send(id, "ERROR - Command not found", protocol)
-  else
-    error("Command not found")
-  end
+  error("Command not found") -- Command not found
 end
 
 function receiveCommands()
   while true do
+    id = nil
     parallel.waitForAny(
                         function()
                           id, input = rednet.receive(protocol)
@@ -116,7 +224,7 @@ function receiveCommands()
                         end
                       )
 
-    executeCommand(command[1])
+    executeCommand(command, registeredCommands)
   end
 end
 
@@ -129,5 +237,5 @@ function main()
   end
 end
 
-updateMonitor()
+setMonitorText()
 main()
